4c34457e0fb0ac01df3ceeb7b4ea14e6c812646d
Himanshu Adhikari <adhikarihimanshu1498@gmail.com>
2020-07-06 22:46:17 +0530

    Create code.cpp

---
+++ Migratory Birds/code.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+// Complete the migratoryBirds function below.
+int migratoryBirds(vector<int> arr) {
+    sort(arr.begin(),arr.end());
+    map <int,int> r;
+    r.insert(pair<int,int>(arr[0],1));
+    int max=r[arr[0]],ele=arr[0];
+    for(int i=1;i<arr.size();i++)
+    {
+        if(r.find(arr[i])==r.end())
+        {
+            r.insert(pair<int,int>(arr[i],1));
+        }
+        else
+        {
+            r[arr[i]]=r[arr[i]]+1;
+            if(r[arr[i]]>max)
+            {
+                max=r[arr[i]];
+                ele=arr[i];
+            }
+        }
+    }
+    return ele;
+
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string arr_count_temp;
+    getline(cin, arr_count_temp);
+
+    int arr_count = stoi(ltrim(rtrim(arr_count_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(arr_count);
+
+    for (int i = 0; i < arr_count; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = migratoryBirds(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
